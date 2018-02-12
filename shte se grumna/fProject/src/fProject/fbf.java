a8d4568ae1a7c91893a30aeb44ba5b842235f521
student <student@ctt2032>
2018-02-12 14:42:28 +0200

    aaaaaaaaaaaaaaa

--- shte se grumna/fProject/src/fProject/fbf.java
+++ shte se grumna/fProject/src/fProject/fbf.java
@@ -5,6 +5,15 @@ public class fbf {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("dfjkrv");
+		System.out.println("dfjkrv");
+		System.out.println("dfjkrv");
+		System.out.println("dfjkrv");
+		System.out.println("dfjkrv");
+		System.out.println("dfjkrv");
+		System.out.println("dfjkrv");
+		System.out.println("dfjkrv");
+		System.out.println("dfjkrv");
+		
 	}
 
 }
