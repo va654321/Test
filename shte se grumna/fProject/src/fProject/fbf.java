3a2eb769bbd2c47576045697f7b361373d9add8b
kpetrov96 <kpetrov96@gmail.com>
2018-02-12 14:43:28 +0200

    napravih promeniiii yeahhhh

--- shte se grumna/fProject/src/fProject/fbf.java
+++ shte se grumna/fProject/src/fProject/fbf.java
@@ -5,6 +5,7 @@ public class fbf {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("dfjkrv");
+		System.out.println("asdjkjksdjksdjkasdf");
 	}
 
 }
