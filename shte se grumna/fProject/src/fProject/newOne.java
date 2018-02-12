5391aeb22b0c160b507f25f0cb5785c6c1316727
kpetrov96 <kpetrov96@gmail.com>
2018-02-12 14:48:27 +0200

    Opit Steliiiiiiiiiiii

--- shte se grumna/fProject/src/fProject/newOne.java
+++
@@ -1,9 +0,0 @@
-package fProject;
-
-public class newOne {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("dfjkrv");
-		System.out.println("asdjasdasd");
-	}
-}
